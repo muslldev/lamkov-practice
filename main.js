2561eef081d79805f9683c201f987f966df5fffa
Muslim Kozhasov <muslim200411@gmail.com>
2025-05-06 11:26:26 +0600

    [*]Fixed a

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 document.querySelector("h1").style.backgroundColor("red");
 const API_URL = "https://jsonplaceholder.typicode.com/posts";
+const API_URL1 = "https://jsonplaceholder.typicode.com/users";
