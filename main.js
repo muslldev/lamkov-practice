7835db1bf733ffd8ec98294bb1da4492d7f57cd3
Muslim Kozhasov <muslim200411@gmail.com>
2025-05-06 11:04:13 +0600

    [+]Added a fetch buttons

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 document.querySelector("h1").style.backgroundColor("red");
 const API_URL = "https://jsonplaceholder.typicode.com/posts";
+const FETCH_BUTTON = document.getElementById("fetch");
+const FETCH_BUTTON2 = document.getElementById("fetch2");
