531ecebc8f1df249ba84df627b192c9159f408a4
Muslim Kozhasov <muslim200411@gmail.com>
2025-05-06 11:13:39 +0600

    Revert "[+]Added a API"
    This reverts commit 5e121f48825b4a0bf90110cad36306bc569327f3.

--- main.js
+++ main.js
@@ -1,4 +1,2 @@
 document.querySelector("h1").style.backgroundColor("red");
 const API_URL = "https://jsonplaceholder.typicode.com/posts";
-const FETCH_BUTTON = document.getElementById("fetch");
-const FETCH_BUTTON2 = document.getElementById("fetch2");
